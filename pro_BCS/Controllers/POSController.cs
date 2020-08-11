6addca44da55f53db816e2afe6d51c2bae989fe9
Rizwan Ahmad <rizwandotahmed@outlook.com>
2020-08-11 06:02:12 -0700

    My first comment

---
+++ pro_BCS/Controllers/POSController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pro_BCS.Controllers
+{
+    public class POSController : Controller
+    {
+        // GET: POS
+        public ActionResult dashboard()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Leftmenu()
+        {
+            return View();
+        }
+        public ActionResult Purchase()
+        {
+            return View();
+        }
+        public ActionResult product()
+        {
+            return View();
+        }
+        public ActionResult Addpro()
+        {
+            return View();
+        }
+    }
+}
